b6acb1d2aa4c8b31c7aa83ba9f11bdd521940fc8
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-07 19:26:26 +0200

    Inject and implement usage of coordAndDateProcessor class

--- SolarWatch/SolarWatch/Controllers/SunRiseSetForCityController.cs
+++ SolarWatch/SolarWatch/Controllers/SunRiseSetForCityController.cs
@@ -8,23 +8,38 @@ public class SunRiseSetForCityController : ControllerBase
 {
     private readonly ILogger<SunRiseSetForCityController> _logger;
     private readonly ICityNameProcessor _cityNameProcessor;
+    private readonly ICoordAndDateProcessor _coordAndDateProcessor;
 
-    public SunRiseSetForCityController(ILogger<SunRiseSetForCityController> logger, ICityNameProcessor cityNameProcessor)
+    public SunRiseSetForCityController(ILogger<SunRiseSetForCityController> logger, ICityNameProcessor cityNameProcessor, ICoordAndDateProcessor coordAndDateProcessor)
     {
         _logger = logger;
         _cityNameProcessor = cityNameProcessor;
+        _coordAndDateProcessor = coordAndDateProcessor;
     }
 
     [HttpGet(Name = "GetSunRiseSetForCity")]
-    public SunRiseSetForCity Get(string cityName, DateTime dateTime)
+    public SunRiseSetForCity Get(string cityName, DateTime date)
     {
-        _logger.LogInformation(_cityNameProcessor.GetLanCoord(cityName).ToString());
+        string formattedDate = date.ToString("yyyy'-'M'-'d");
+        
+        // getting coordinates based on city name
+        var lat = _cityNameProcessor.GetLatCoord(cityName);
+        var lon = _cityNameProcessor.GetLonCoord(cityName);
+        
+        _logger.LogInformation($"Data from _cityNameProcessor --- LAT:{lat}, LON:{lon}");
+        
+        // using coordinates to get times of sunrise/set
+        var sunrise = _coordAndDateProcessor.GetSunriseTime(lat, lon, formattedDate);
+        var sunset = _coordAndDateProcessor.GetSunsetTime(lat, lon, formattedDate);
+        
+        _logger.LogInformation($"Data from _coordAndDateProcessor --- RISE:{sunrise}, SET:{sunset}");
+        
         return new SunRiseSetForCity
         {
             CityName = cityName,
-            Date = DateTime.Today,
-            SunRise = DateTime.Now,
-            SunSet = DateTime.MaxValue,
+            Date = date,
+            SunRise = sunrise,
+            SunSet = sunset,
         };
 
     }
