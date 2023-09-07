988192e71ffe1d3c8c0e3146f9d2a821d6ec8751
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-07 13:41:24 +0200

    Add basic functionality to SunRiseSetForCityController.cs

---
+++ SolarWatch/SolarWatch/Controllers/SunRiseSetForCityController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolarWatch.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class SunRiseSetForCityController : ControllerBase
+{
+    private readonly ILogger<SunRiseSetForCityController> _logger;
+
+    public SunRiseSetForCityController(ILogger<SunRiseSetForCityController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetSunRiseSetForCity")]
+    public SunRiseSetForCity Get(string cityName)
+
+    {
+        return new SunRiseSetForCity
+        {
+            CityName = cityName,
+            Date = DateTime.Today,
+            SunRise = DateTime.Now,
+            SunSet = DateTime.MaxValue,
+        };
+
+    }
+}
