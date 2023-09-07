806096391101de4161ec3539870267bc604ab55f
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-07 13:41:59 +0200

    Create SunRiseSetForCity.cs

---
+++ SolarWatch/SolarWatch/SunRiseSetForCity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolarWatch;
+
+public class SunRiseSetForCity
+{
+    public string CityName { get; set; }
+    public DateTime Date { get; set; }
+    public DateTime SunSet { get; set; }
+    public DateTime SunRise { get; set; }
+}
