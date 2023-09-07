56dbe08e6bb9c59518a615d2073b2764afabcf2f
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-07 15:05:12 +0200

    Registering the CityNameProcessor as a singleton dependency

--- SolarWatch/SolarWatch/Program.cs
+++ SolarWatch/SolarWatch/Program.cs
@@ -1,3 +1,5 @@
+using SolarWatch.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// registering interface(s) and their implementations in the DI container
+builder.Services.AddSingleton<ICityNameProcessor, CityNameProcessor>();
 
 var app = builder.Build();
 
