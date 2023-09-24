cacf61bda14ba41ccc17f8e0a6614d26bd832eae
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-24 14:51:10 +0200

    feat: create ISunTimesRepository.cs

---
+++ SolarWatch/SolarWatch/Services/Repository/ISunTimesRepository.cs
@@ -0,0 +1,14 @@
+﻿using SolarWatch.Model;
+
+namespace SolarWatch.Services.Repository;
+
+public interface ISunTimesRepository
+{
+    IEnumerable<SunTimes> GetAll();
+    SunTimes? GetByCityName(string cityName);
+    SunTimes? GetByDateAndName(string cityName, DateTime date);
+
+    void Add(SunTimes sunTimes);
+    void Delete(SunTimes sunTimes);
+    void Update(SunTimes sunTimes);
+}
