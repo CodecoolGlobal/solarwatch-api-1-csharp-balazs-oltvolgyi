13be8660025b63ec22f71bfb9a8ac44db01c45a4
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-24 14:50:53 +0200

    fix: change float types to double

--- SolarWatch/SolarWatch/Services/ICoordAndDateProcessor.cs
+++ SolarWatch/SolarWatch/Services/ICoordAndDateProcessor.cs
@@ -2,6 +2,6 @@
 
 public interface ICoordAndDateProcessor
 {
-    public Task<string> GetSunriseTime(float lat, float lon, string date);
-    public Task<string> GetSunsetTime(float lat, float lon, string date);
+    public Task<string> GetSunriseTime(double lat, double lon, string date);
+    public Task<string> GetSunsetTime(double lat, double lon, string date);
 }
