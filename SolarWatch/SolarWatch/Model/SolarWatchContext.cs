f8dd45281f06dbcbc965f379a839bc68701a2cf1
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-24 14:52:48 +0200

    feat: implement SolarWatchContext.cs

---
+++ SolarWatch/SolarWatch/Model/SolarWatchContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SolarWatch.Model;
+
+public class SolarWatchContext : DbContext
+{
+    public DbSet<City> Cities { get; set; }
+    public DbSet<SunTimes> SunTimes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost,1433;Database=SolarWatch;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=True");
+    }
+    
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        //Configure the City entity - making the 'Name' unique
+        builder.Entity<City>()
+            .HasIndex(u => u.Name)
+            .IsUnique();
+        
+        builder.Entity<SunTimes>()
+            .HasIndex(u => u.SunSet)
+            .IsUnique();
+    
+
+    }
+
+}
