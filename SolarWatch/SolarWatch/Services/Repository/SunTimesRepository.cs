547b77ba0d6174b2dfd04b40d993b9d750c4dd83
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-24 14:55:40 +0200

    feat: implement SunTimesRepository.cs with basic CRUD options

---
+++ SolarWatch/SolarWatch/Services/Repository/SunTimesRepository.cs
@@ -0,0 +1,45 @@
+﻿using SolarWatch.Model;
+
+namespace SolarWatch.Services.Repository;
+
+public class SunTimesRepository : ISunTimesRepository
+{
+    public IEnumerable<SunTimes> GetAll()
+    {
+        using var dbContext = new SolarWatchContext();
+        return dbContext.SunTimes.ToList();
+    }
+
+    public SunTimes? GetByCityName(string cityName)
+    {
+        using var dbContext = new SolarWatchContext();
+        return dbContext.SunTimes.FirstOrDefault(c => c.CityName == cityName);
+    }
+
+    public SunTimes? GetByDateAndName(string cityName, DateTime date)
+    {
+        using var dbContext = new SolarWatchContext();
+        return dbContext.SunTimes.FirstOrDefault(s => s.Date == date && s.CityName == cityName);
+    }
+
+    public void Add(SunTimes city)
+    {
+        using var dbContext = new SolarWatchContext();
+        dbContext.Add(city);
+        dbContext.SaveChanges();
+    }
+
+    public void Delete(SunTimes city)
+    {
+        using var dbContext = new SolarWatchContext();
+        dbContext.Remove(city);
+        dbContext.SaveChanges();
+    }
+
+    public void Update(SunTimes city)
+    {  
+        using var dbContext = new SolarWatchContext();
+        dbContext.Update(city);
+        dbContext.SaveChanges();
+    }
+}
