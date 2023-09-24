a55fbd93b359ab940153e386412a32926eb3cb01
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-09-24 14:54:52 +0200

    feat: create SunTimes.cs

---
+++ SolarWatch/SolarWatch/Model/SunTimes.cs
@@ -0,0 +1,10 @@
+﻿namespace SolarWatch.Model;
+
+public class SunTimes
+{
+    public int Id { get; init; }
+    public string? CityName { get; set; }
+    public DateTime SunRise { get; set; }
+    public DateTime SunSet { get; set; }
+    public DateTime Date { get; set; }
+}
