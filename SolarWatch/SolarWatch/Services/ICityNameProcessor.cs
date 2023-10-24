c21f6398631fac6c768f3c9e7dfb021623f5be9e
balazs-oltvolgyi <balazs.oltvolgyi@gmail.com>
2023-10-24 10:00:37 +0200

    fix: include common coord-getter method signature

--- SolarWatch/SolarWatch/Services/ICityNameProcessor.cs
+++ SolarWatch/SolarWatch/Services/ICityNameProcessor.cs
@@ -2,6 +2,7 @@
 
 public interface ICityNameProcessor
 {
+    public Task<(double, double)> GetCoords(string cityName);
     public Task<double> GetLatCoord(string cityName);
     public Task<double> GetLonCoord(string cityName);
     public Task<string> GetCountry(string cityName);
